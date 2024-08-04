1a9f84bf56914d5d6289c88d163072b116292f57
Felipe-Tomazoti <felipecesar005@gmail.com>
2024-08-03 22:27:24 -0300

    Consulta simples com query methods

--- SpringBoot/src/main/java/com/workshop/SpringBoot/resources/PostResource.java
+++ SpringBoot/src/main/java/com/workshop/SpringBoot/resources/PostResource.java
@@ -1,18 +1,12 @@
 package com.workshop.SpringBoot.resources;
-
 import com.workshop.SpringBoot.domain.Post;
-import com.workshop.SpringBoot.domain.User;
-import com.workshop.SpringBoot.dto.UserDTO;
+import com.workshop.SpringBoot.resources.util.URL;
 import com.workshop.SpringBoot.services.PostService;
-import com.workshop.SpringBoot.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -26,4 +20,12 @@ public class PostResource {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
+    }
+
 }
